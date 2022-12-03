8a858b3410ad310fea4877fdc42c7e0b1fc997ad
dmytro40a <extremesoroka@gmail.com>
2022-12-04 01:58:44 +0200

    Added HW #16

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Contacts from "./components/Contacts";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import maleIcon from "../assets/male.png";
+import femaleIcon from "../assets/female.png";
+import unknownIcon from "../assets/unknown.png";
+
+let gender;
+
+class Contact extends Component {
+  render() {
+    function genderIcon(gender) {
+      if (gender === "male") {
+        return maleIcon;
+      }
+      if (gender === "female") {
+        return femaleIcon;
+      } else return unknownIcon;
+    }
+    return (
+      <div className="contact-card">
+        <h2 key={`key is ${this.props}${(gender = this.props.gender)}`}>
+          {this.props.firstName} {this.props.lastName}
+        </h2>
+        <p>{this.props.phone}</p>
+        <img
+          className="gender_icon"
+          src={genderIcon(gender)}
+          alt={gender + "-icon"}
+        />
+      </div>
+    );
+  }
+}
+
+export default Contact;
